c679f28b369384c7fdd812108ecfdab0c17e73ea
alarqabandanyah-alt <alarqabandanyah@gmail.com>
2026-06-24 14:08:13 -0700

    Add python profile lab

---
+++ profile.py
@@ -0,0 +1,24 @@
+# Danyah Alarqaban | Lab 3 | Intro to Python
+full_name = "Danyah Alarqaban"
+nickname="Danyah"
+age= 14
+used_python_before = True   
+Hobbies = ["Shopping","watching movies","Hanging out with friends"]
+faviorties = {
+    "color": "blue",
+    "food": "ceasar salad",
+    "movie": "The Hunger Games",
+      "hobby": "Shopping"
+}   
+skills = ("Python", "teamwork")
+lanuguages_spoken = ["English", "Arabic"]
+
+print(full_name)
+print(nickname)
+print(age)
+print(used_python_before)
+print(Hobbies)
+print(faviorties)
+print(skills)
+print(lanuguages_spoken)
+
